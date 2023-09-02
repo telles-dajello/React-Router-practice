6101859e322476a773dd4c6fcc5a321d9b586cf8
telles-dajello <telles.dajello@gmail.com>
2023-09-02 02:08:04 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+
+// Add react-router-dom imports
+import { RouterProvider,createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+// create router with JSX Route elements
+const appRouter = "REPLACE ME";
+
+function App() {
+  return (
+  <RouterProvider router={appRouter} />
+  );
+}
+
+export default App;
